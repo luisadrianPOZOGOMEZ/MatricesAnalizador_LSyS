a1c735f3238b880b8e3136933a0721bdbd8609ce
luisadrianPOZOGOMEZ <luispozoengineer@gmail.com>
2024-07-22 09:36:08 -0600

    second commit

--- app.py
+++ app.py
@@ -234,4 +234,4 @@ def index():
                            syntax_error=syntax_error, semantic_errors=semantic_errors, totals=totals, result=result)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
